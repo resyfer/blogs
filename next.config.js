8475b253223224a2437c8d5aeb1e7ff1368cf074
Saurav Pal <resyfer.dev@gmail.com>
2024-05-20 00:21:54 +0530

    Nextjs config out

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 const withNextra = require('nextra')({
     theme: 'nextra-theme-blog',
     themeConfig: './theme.config.jsx',
-    output: "export"
 })
 
-module.exports = withNextra()
+
+module.exports = {
+    ...withNextra(),
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
+}
