87410fafa204c2a32facee9be543b4578ce18e62
Saurav Pal <resyfer.dev@gmail.com>
2024-05-19 23:45:23 +0530

    Intro I

---
+++ theme.config.jsx
@@ -0,0 +1,13 @@
+export default {
+  footer: <p>MIT 2024 © resyfer.</p>,
+  head: ({ title, meta }) => (
+    <>
+      {meta.description && (
+        <meta name="description" content={meta.description} />
+      )}
+      {meta.tag && <meta name="keywords" content={meta.tag} />}
+      {meta.author && <meta name="author" content={meta.author} />}
+    </>
+  ),
+  postFooter: false,
+}
