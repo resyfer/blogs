1c23bde141949bbaacd3e78fd14581a5f25410e2
Saurav Pal <resyfer.dev@gmail.com>
2024-05-19 23:53:30 +0530

    Deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withNextra = require('nextra')({
     theme: 'nextra-theme-blog',
-    themeConfig: './theme.config.jsx'
+    themeConfig: './theme.config.jsx',
+    output: "export"
 })
 
 module.exports = withNextra()
