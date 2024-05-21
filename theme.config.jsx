d91e093d7384c4effea3a91ff7ee5acf7b5c8f7e
Saurav Pal <resyfer.dev@gmail.com>
2024-05-21 19:53:07 +0530

    Intro P3

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,3 +1,5 @@
+import Script from "next/script"
+
 export default {
   footer: <p>MIT 2024 © resyfer.</p>,
   head: ({ title, meta }) => (
@@ -7,6 +9,17 @@ export default {
       )}
       {meta.tag && <meta name="keywords" content={meta.tag} />}
       {meta.author && <meta name="author" content={meta.author} />}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-HFNGDFK1PN"></Script>
+      <Script>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-HFNGDFK1PN');
+        `}
+      </Script>
+
     </>
   ),
   postFooter: false,
